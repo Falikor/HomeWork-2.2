c1f85bae890d554f5dd7df272422a15442b07c5a
Falikor <Bugorv90@mail.ru>
2021-05-21 13:05:26 +0300

    Create work code with alpha

--- HomeWork-2.2/ViewController.swift
+++ HomeWork-2.2/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// added utlet View, Labels, Sliders
+    
     @IBOutlet weak var viewPrint: UIView!
     @IBOutlet weak var redLabel: UILabel! 
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var alfaLabel: UILabel!
+    
+// Added start value and color
     
     @IBOutlet weak var rSlider: UISlider! {
         didSet {
@@ -21,13 +25,14 @@ class ViewController: UIViewController {
             rSlider.value = 0
         }
     }
+    
     @IBOutlet weak var gSlaider: UISlider! {
         didSet {
             gSlaider.tintColor = UIColor.green
             gSlaider.value = 0
         }
     }
-    
+
     @IBOutlet weak var bSlaider: UISlider! {
         didSet {
             bSlaider.tintColor = UIColor.blue
@@ -42,31 +47,25 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+// creat func change color and alpha
     private func changColor() {
         viewPrint.backgroundColor = UIColor(displayP3Red: CGFloat(rSlider.value), green: CGFloat(gSlaider.value), blue: CGFloat(bSlaider.value), alpha: CGFloat(aSlaider.value))
     }
     
+// creat action sliders wich use func change color and sed valeus in label
     @IBAction func redSender(_ sender: UISlider) {
         changColor()
         redLabel.text = String((sender.value * 100).rounded() / 100)
         
     }
-    
     @IBAction func greenSender(_ sender: UISlider) {
         changColor()
         greenLabel.text = String((sender.value * 100).rounded() / 100)
     }
-    
     @IBAction func blueSender(_ sender: UISlider) {
         changColor()
         blueLabel.text = String((sender.value * 100).rounded() / 100)
     }
-    
     @IBAction func alfaSender(_ sender: UISlider) {
         changColor()
         alfaLabel.text = String((sender.value * 100).rounded() / 100)
